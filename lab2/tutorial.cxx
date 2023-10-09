31602656f29ebf71aafd0414e401797d73611eef
tudor21211 <tudor.androne@student.upt.ro>
2023-10-09 21:46:32 +0300

    Added source files

---
+++ lab2/tutorial.cxx
@@ -0,0 +1,25 @@
+// A simple program that computes the square root of a number
+#include <cmath>
+#include <iostream>
+#include <string>
+#include "TutorialConfig.h"
+
+
+int main(int argc, char* argv[])
+{
+  if (argc < 2) {
+
+    std::cout<< "Tutorial_VERSION_MAJOR" << Tutorial_VERSION_MAJOR<<std::endl;
+    std::cout<< "Tutorial_VERSION_MINOR" << Tutorial_VERSION_MINOR<<std::endl;
+    std::cout << "Usage: " << argv[0] << " number" << std::endl;
+    return 1;
+  }
+
+
+  const double inputValue = std::stod(argv[1]);
+
+  const double outputValue = sqrt(inputValue);
+  std::cout << "The square root of " << inputValue << " is " << outputValue
+            << std::endl;
+  return 0;
+}
