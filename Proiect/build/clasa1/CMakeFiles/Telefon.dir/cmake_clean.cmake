988adbc6fe258b838d6f01c1f005800483753575
tudor21211 <tudor.androne@student.upt.ro>
2023-10-29 20:48:34 +0200

    added the build directory

---
+++ Proiect/build/clasa1/CMakeFiles/Telefon.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Telefon.dir/Telefon.cpp.o"
+  "CMakeFiles/Telefon.dir/Telefon.cpp.o.d"
+  "libTelefon.a"
+  "libTelefon.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Telefon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
